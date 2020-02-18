6a3d638803840726ff02f8c1fd88c0e2343cb5a1
kkm8257 <57127381+kkm8257@users.noreply.github.com>
2020-02-18 19:04:09 +0900

    Update Q11720.java

--- java_tutorials/src/BaekProblem/Q11720.java
+++ java_tutorials/src/BaekProblem/Q11720.java
@@ -1,22 +1,38 @@
 package BaekProblem;
 
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import java.util.StringTokenizer;
 public class Q11720 {
 
-	public static void main(String[] args) {
-		
+	public static void main(String[] args)throws IOException {
 		int result=0;
+		BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
 		
-		Scanner sc= new Scanner(System.in);
-		int temp=sc.nextInt();
-		String Str_temp=Integer.toString(temp);
+
+		String count=br.readLine();
+		String Str_temp=br.readLine();
+
 		
-		String[] Array=Str_temp.split("");
-		for(int i =0;i<Array.length;i++) {
-			result=result+Integer.parseInt(Array[i]);
+		
+		
+		
+		if((Integer.parseInt(count)>0)&&(Integer.parseInt(count)<=100)) {
+			
+			String[] Array=Str_temp.split("");
+			for(int i=0;i<Array.length;i++) {
+				
+				result=result+Integer.parseInt(Array[i]);
+			}
 			
 		}
+		else {
+			System.out.println("¿À·ù");
+		}
+
 		System.out.println(result);
 	}
 
